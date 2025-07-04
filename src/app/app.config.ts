db22f10891983836a9a17cbd510f1fe6a4dad80e
mzkmnk <sma0729mex@gmail.com>
2025-07-05 08:25:03 +0900

    feat: アイコン管理の改善とSVGファイルの削除
    主な変更:
    - @ng-icons/coreと@ng-icons/tabler-iconsを使用してアイコン管理を改善
    - IconComponentをスタンドアロンコンポーネントとして再設計し、NgIconをインポート
    - 既存のSVGアイコンファイルを削除し、アイコン名をマッピングする方式に変更

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
+import {
+  tablerMenu2,
+  tablerMoon,
+  tablerMessage,
+  tablerSettings,
+  tablerHistory,
+  tablerPaperclip,
+  tablerSend,
+  tablerFile,
+  tablerX,
+} from '@ng-icons/tabler-icons';
 
 import { routes } from './app.routes';
 
@@ -7,6 +23,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideIcons({
+      tablerMenu2,
+      tablerMoon,
+      tablerMessage,
+      tablerSettings,
+      tablerHistory,
+      tablerPaperclip,
+      tablerSend,
+      tablerFile,
+      tablerX,
+    }),
+  ],
 };
